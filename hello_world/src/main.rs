c9608af6faa0cd484c42729cbecdf78cdf522747
Yin Gang <zenith.yin@gmail.com>
2019-05-04 16:09:17 +0800

    hello custom types

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,68 +1,214 @@
 use std::fmt;
 
-fn main() {
-    // 1
+fn chapter1() {
     println!("Hello, world!");
     println!("I'm a Rustacean!");
+}
 
-    // 1.1
+fn chapter1_1() {
     let x = 5 + /* 90 + */ 5;
     println!("Is `x` 10 or 100? x = {}", x);
 
     println!("{} days", 31);
     println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
     println!("{subject} {verb} {object}",
-             object="the lazy dog",
-             subject="the quick brown fox",
-             verb="jumps over");
-    println!("{} of {:b} people know binary, the other half don't", 1, 2);
-    println!("{number:>width$}", number=1, width=6);
-    println!("{number:>0width$}", number=1, width=6);
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);  // 二进制输出
+    println!("{number:>width$}", number = 1, width = 6);    // 指定宽度
+    println!("{number:>0width$}", number = 1, width = 6);   // 前补零
     println!("My name is {0}, {1} {0}", "Bond", "James");
-    
-    // 1.2
+}
+
+fn chapter1_2() {
     #[allow(dead_code)]
-    #[derive(Debug)]
+    #[derive(Debug)]    // 有这个就可以用 {:?} 了
     struct Structure(i32);
 
-    println!("This struct `{:?}` won't print...", Structure(3));
+    println!("This struct `{:?}` won't print...fixed", Structure(3));
 
-    impl fmt::Display for Structure{
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "Structure({})", self.0)  // 为什么这里不能有分号？
+    impl fmt::Display for Structure {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {  // 这个方法的签名怎么理解？
+            // f 看上去是一个缓冲区
+            write!(f, "Structure({})", self.0) // 结尾没有 `;` 表示直接返回 write! 的返回值吧
         }
     }
 
-    println!("This struct `{}` won't print...", Structure(3));
+    println!("This struct `{}` won't print...fixed", Structure(3));
 
     let pi = 3.141592;
     println!("Pi is roughly {:.3}", pi);
+}
 
-    // 1.2.1
+fn chapter1_2_1() {
+    #[derive(Debug)]
+    struct Structure(i32);
+
     #[derive(Debug)]
     struct Deep(Structure);
 
     println!("{:?} months in a year.", 12);
     println!("{1:?} {0:?} is the {actor:?} name.",
-             "Slater",
-             "Christian",
-             actor="actor's");
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
 
     println!("Now {:?} will print!", Structure(3));
-    
+
     println!("Now {:?} will print!", Deep(Structure(7)));
 
     #[derive(Debug)]
-    struct Person<'a> {     // 这个 ' 是啥意思？
+    struct Person<'a> {     // 这个 'a 是表示生命周期吗？
         name: &'a str,      // 这个 & 呢？
-        age: u8
+        age: u8,
     }
 
     let name = "Peter";
     let age = 27;
     let peter = Person { name, age };
+
+    // 美化打印
     println!("{:#?}", peter);
+}
 
-    // 1.2.2
+fn chapter1_2_2() {
+    #[derive(Debug)]
+    struct MinMax(i64, i64);
 
+    impl fmt::Display for MinMax {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({}, {})", self.0, self.1)
+        }
+    }
+
+    #[derive(Debug)]
+    struct Point2D {
+        x: f64,
+        y: f64,
+    }
+
+    impl fmt::Display for Point2D {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "x: {}, y: {}", self.x, self.y)
+        }
+    }
+
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!(
+        "The big range is {big} and the small is {small}",
+        small = small_range,
+        big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    #[derive(Debug)]
+    struct Complex {
+        real: f64,
+        imag: f64,
+    }
+
+    impl fmt::Display for Complex {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{} + {}i", self.real, self.imag)
+        }
+    }
+
+    let complex = Complex { real: 3.3, imag: 7.2 };
+
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
+}
+
+fn chapter1_2_2_1() {
+    struct List(Vec<i32>);
+
+    impl fmt::Display for List {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let vec = &self.0;
+            write!(f, "[")?;    // 有 ? 表示活没干完，后续继续
+            for (count, v) in vec.iter().enumerate() {
+                if count != 0 { write!(f, ", ")?; }
+                write!(f, "{}: {}", count, v)?;
+            }
+            write!(f, "]")      // 无 ? 表示活干完了，同前，不需要用 `;` 结尾
+        }
+    }
+
+    let v = List(vec![1, 2, 3]);    // 这个 vec! 又是什么？
+    println!("{}", v);
+}
+
+fn chapter1_3() {
+    use std::fmt::{self, Formatter, Display};
+
+    struct City {
+        name: &'static str,
+        lat: f32,
+        lon: f32,
+    }
+
+    impl Display for City {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+            write!(f, "{}: {:.3}°{} {:.3}°{}",
+                self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+        }
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+
+    impl Display for Color {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            write!(f, "RGB ({}, {}, {}) 0x{:02X}{:02X}{:02X}",
+                self.red, self.green, self.blue,
+                self.red, self.green, self.blue)
+        }
+    }
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        println!("{}", *color)
+    }
+}
+
+fn main() {
+    chapter1();
+    chapter1_1();
+    chapter1_2();
+    chapter1_2_1();
+    chapter1_2_2();
+    chapter1_2_2_1();
+    chapter1_3();
 }
---
+++ primitive/src/main.rs
@@ -0,0 +1,142 @@
+ #[allow(unused)]
+ fn chapter2() {
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;  // 常规说明
+    let an_integer   = 5i32; // 后缀说明
+
+    // 否则会按默认方式决定类型。
+    let default_float   = 3.0; // `f64`
+    let default_integer = 7;   // `i32`
+    
+    // 类型也可根据上下文自动推断。
+    let mut inferred_type = 12; // 根据下一行的赋值推断为 i64 类型
+    inferred_type = 4294967296i64;
+    
+    // 可变的（mutable）变量，其值可以改变。
+    let mut mutable = 12; // Mutable `i32`
+    mutable = 21;
+    
+    // 报错！变量的类型并不能改变。
+    // mutable = true;
+    
+    // 但可以用掩蔽（shadow）来覆盖前面的变量。
+    let mutable = true;
+}
+
+fn chapter2_1() {
+    println!("1 + 2 = {}", 1u32 + 2);
+    println!("1 - 2 = {}", 1i32 - 2);
+
+    // overflow 检查，厉害了
+    // println!("1 - 2 = {}", 1u32 - 2);
+
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);    // 不写 u32 后缀也是可以的
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // 使用下划线改善数字的可读性！
+    println!("One million is written as {}", 1_000_000u32);
+}
+
+fn chapter2_2() {
+    // 元组可以充当函数的参数和返回值
+    fn reverse(pair: (i32, bool)) -> (bool, i32) {
+        let (integer, boolean) = pair;
+        (boolean, integer)  // 看来这玩意的含义是 return (boolean, integer);
+    }
+
+    #[derive(Debug)]
+    struct Matrix(f32, f32, f32, f32);
+
+    // 包含各种不同类型的元组
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+
+    // 通过元组的下标来访问具体的值
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+    
+    // 但很长的元组无法打印
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);    // 提示 doesn't implement `std::fmt::Debug`
+    
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    println!("one element tuple: {:?}", (5u32,));   // 注意额外的逗号！
+    println!("just an integer: {:?}", (5u32));
+
+    let tuple = (1, "hello", 4.5, true);
+    let (a, b, c, d) = tuple;   // 解构 | deconstruct
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+
+    use std::fmt;
+    impl fmt::Display for Matrix {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({}, {})\n({}, {})", self.0, self.1, self.2, self.3)
+        }
+    }
+
+    fn transpose(matrix: Matrix) -> Matrix {
+        Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+    }
+
+    println!("{}", matrix);
+    println!("{}", transpose(matrix));
+}
+
+fn chapter2_3() {
+    use std::mem;
+
+    // 此函数借用一个 slice
+    fn analyze_slice(slice: &[i32]) {   // &[T] 是 slice 的类型标记！
+        println!("first element of the slice: {}", slice[0]);
+        println!("the slice has {} elements", slice.len());
+    }
+
+    let xs/*: [i32; 5]*/ = [1, 2, 3, 4, 5]; // 可以不写类型标记
+
+    let ys: [i32; 500] = [0; 500];      // 这句的意思是全部初始化成 0 ？
+
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+    println!("array size: {}", xs.len());
+
+    // 数组是在栈中分配的
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+    // 数组可以自动被借用成为 slice
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    // slice 可以指向数组的一部分
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1 .. 4]);
+
+    // println!("{}", xs[5]);  // 编译器会提示：index out of bounds: the len is 5 but the index is 5
+}
+
+fn main() {
+    chapter2();
+    chapter2_1();
+    chapter2_2();
+    chapter2_3();
+}
