715fd23327db5b0b7f5b2f3bdf113078b65ba54b
Yin Gang <zenith.yin@gmail.com>
2020-02-10 21:30:07 +0800

    here comes chapter 9

---
+++ fn/src/main.rs
@@ -0,0 +1,139 @@
+fn chapter9_0() {
+    // 不需要在定义之后才使用！
+    fizzbuzz_to(0xf);
+
+    fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+        if rhs == 0 {
+            return false;
+        }
+
+        lhs % rhs == 0
+    }
+
+    // 一个 “不” 返回值的函数。实际上会返回一个单元类型 `()`。
+    fn fizzbuzz(n: u32) -> () {
+        if is_divisible_by(n, 15) {
+            println!("fizzbuzz");
+        } else if is_divisible_by(n, 3) {
+            println!("fizz");
+        } else if is_divisible_by(n, 5) {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // 当函数返回 `()` 时，函数签名可以省略返回类型
+    fn fizzbuzz_to(n: u32) {
+        for n in 1..n + 1 {
+            fizzbuzz(n);
+        }
+    }
+}
+
+fn chapter9_1() {
+    struct Point {
+        x: f64,
+        y: f64,
+    }
+
+    // 通过 impl 语句块来定义 Point 结构的方法
+    impl Point {
+        // 没有定义 self 参数的是静态方法（static method）
+        fn origin() -> Point {
+            Point { x: 0.0, y: 0.0 }
+        }
+
+        // 静态方法可以有参数
+        fn new(x: f64, y: f64) -> Point {
+            Point { x: x, y: y }
+        }
+    }
+
+    struct Rectangle {
+        p1: Point,
+        p2: Point,
+    }
+
+    impl Rectangle {
+        // 实例方法（instance method）
+        // `&self` 是 `self: &Self` 的语法糖（sugar），其中 `Self` 是方法调用者的类型
+        fn area(&self) -> f64 {
+            // `self` 通过点运算符来访问结构体字段
+            let Point { x: x1, y: y1 } = self.p1;
+            let Point { x: x2, y: y2 } = self.p2;
+
+            // `abs` 是一个 `f64` 类型的方法，返回调用者的绝对值
+            ((x1 - x2) * (y1 - y2)).abs()
+        }
+
+        fn perimeter(&self) -> f64 {
+            let Point { x: x1, y: y1 } = self.p1;
+            let Point { x: x2, y: y2 } = self.p2;
+
+            2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+        }
+
+        // 这个方法要求调用者是可变的
+        // `&mut self` 为 `self: &mut Self` 的语法糖
+        fn translate(&mut self, x: f64, y: f64) {
+            self.p1.x += x;
+            self.p2.x += x;
+
+            self.p1.y += y;
+            self.p2.y += y;
+        }
+    }
+
+    // `Pair` 拥有资源：两个堆分配的整型
+    struct Pair(Box<i32>, Box<i32>);
+
+    impl Pair {
+        // 这个方法会 “消耗” 调用者的资源
+        // `self` 为 `self: Self` 的语法糖
+        fn destroy(self) {
+            // 解构 `self`
+            let Pair(first, second) = self;
+
+            println!("Destroying Pair({}, {})", first, second);
+
+            // `first` 和 `second` 离开作用域后释放
+        }
+    }
+
+
+    let rectangle = Rectangle {
+        // 静态方法使用双冒号调用
+        p1: Point::origin(),
+        p2: Point::new(3.0, 4.0),
+    };
+
+    // 实例方法通过点运算符来调用
+    // 注意第一个参数 `&self` 是隐式传递的，亦即：
+    // `rectangle.perimeter()` === `Rectangle::perimeter(&rectangle)`
+    println!("Rectangle perimeter: {}", rectangle.perimeter());
+    println!("Rectangle area: {}", rectangle.area());
+
+    let mut square = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(1.0, 1.0),
+    };
+
+    // 报错！ `rectangle` 是不可变的，但这方法需要一个可变对象
+    //rectangle.translate(1.0, 0.0);
+
+    // 正常运行！可变对象可以调用可变方法
+    square.translate(1.0, 1.0);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+
+    pair.destroy();
+
+    // 报错！前面的 `destroy` 调用 “消耗了” `pair`
+    //pair.destroy();
+}
+
+fn main() {
+    chapter9_0();
+    chapter9_1();
+}
