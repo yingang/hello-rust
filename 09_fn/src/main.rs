7a775d0a5539c4f7083eac48d870b26f8d34ca1d
Yin Gang <zenith.yin@gmail.com>
2020-02-21 17:47:01 +0800

    finish chapter 9

--- 09_fn/src/main.rs
+++ 09_fn/src/main.rs
@@ -101,7 +101,6 @@ fn chapter9_1() {
         }
     }
 
-
     let rectangle = Rectangle {
         // 静态方法使用双冒号调用
         p1: Point::origin(),
@@ -162,8 +161,349 @@ fn chapter9_2()
     println!("closure returning one: {}", one());
 }
 
+fn chapter9_2_1()
+{
+    use std::mem;
+    
+    let color = "green";
+
+    // 这个闭包打印 `color`。它会立即借用（通过引用，`&`）`color` 并将该借用和
+    // 闭包本身存储到 `print` 变量中。`color` 会一直保持被借用状态直到
+    // `print` 离开作用域。
+    // `println!` 只需传引用就能使用，而这个闭包捕获的也是变量的引用，因此无需
+    // 进一步处理就可以使用 `println!`。
+    let print = || println!("`color`: {}", color);
+
+    // 调用闭包，闭包又借用 `color`。
+    print();
+    print();
+
+    let mut count = 0;
+
+    // 这个闭包使 `count` 值增加。要做到这点，它需要得到 `&mut count` 或者
+    // `count` 本身，但 `&mut count` 的要求没那么严格，所以我们采取这种方式。
+    // 该闭包立即借用 `count`。
+    //
+    // `inc` 前面需要加上 `mut`，因为闭包里存储着一个 `&mut` 变量。调用闭包时，
+    // 该变量的变化就意味着闭包内部发生了变化。因此闭包需要是可变的。
+    let mut inc = || {
+        count += 1;
+        println!("`count`: {}", count);
+    };
+
+    // 调用闭包。
+    inc();
+    inc();
+
+    //let reborrow = &mut count;
+    // ^ 试一试：将此行注释去掉。
+    
+    // 不可复制类型（non-copy type）。
+    let movable = Box::new(3);
+
+    // `mem::drop` 要求 `T` 类型本身，所以闭包将会捕获变量的值。这种情况下，
+    // 可复制类型将会复制给闭包，从而原始值不受影响。不可复制类型必须移动
+    // （move）到闭包中，因而 `movable` 变量在这里立即移动到了闭包中。
+    let consume = || {
+        println!("`movable`: {:?}", movable);
+        mem::drop(movable);
+    };
+
+    // `consume` 消耗了该变量，所以该闭包只能调用一次。
+    consume();
+    //consume();
+    // ^ 试一试：将此行注释去掉。
+
+
+    // `Vec` 在语义上是不可复制的。
+    let haystack = vec![1, 2, 3];
+
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
+
+    //println!("There're {} elements in vec", haystack.len());
+    // ^ 取消上面一行的注释将导致编译时错误，因为借用检查不允许在变量被移动走
+    // 之后继续使用它。
+    
+    // 在闭包的签名中删除 `move` 会导致闭包以不可变方式借用 `haystack`，因此之后
+    // `haystack` 仍然可用，取消上面的注释也不会导致错误。
+}
+
+// 突然出现的泛型，超纲了吧！
+fn chapter9_2_2()
+{
+    // 该函数将闭包作为参数并调用它。
+    fn apply<F>(f: F) where
+        // 闭包没有输入值和返回值。
+        F: FnOnce() {
+        // ^ 试一试：将 `FnOnce` 换成 `Fn` 或 `FnMut`。
+
+        f();
+    }
+
+    // 输入闭包，返回一个 `i32` 整型的函数。
+    fn apply_to_3<F>(f: F) -> i32 where
+        // 闭包处理一个 `i32` 整型并返回一个 `i32` 整型。
+        F: Fn(i32) -> i32 {
+
+        f(3)
+    }
+
+    use std::mem;
+    
+    let greeting = "hello";
+    // 不可复制的类型。
+    // `to_owned` 从借用的数据创建有所有权的数据。
+    let mut farewell = "goodbye".to_owned();
+
+    // 捕获 2 个变量：通过引用捕获 `greeting`，通过值捕获 `farewell`。
+    let diary = || {
+        // `greeting` 通过引用捕获，故需要闭包是 `Fn`。
+        println!("I said {}.", greeting);
+
+        // 下文改变了 `farewell` ，因而要求闭包通过可变引用来捕获它。
+        // 现在需要 `FnMut`。
+        farewell.push_str("!!!");
+        println!("Then I screamed {}.", farewell);
+        println!("Now I can sleep. zzzzz");
+
+        // 手动调用 drop 又要求闭包通过值获取 `farewell`。
+        // 现在需要 `FnOnce`。
+        mem::drop(farewell);
+    };
+
+    // 以闭包作为参数，调用函数 `apply`。
+    apply(diary);
+
+    // 闭包 `double` 满足 `apply_to_3` 的 trait 约束。
+    let double = |x| 2 * x;
+
+    println!("3 doubled: {}", apply_to_3(double));
+}
+
+fn chapter9_2_3()
+{
+    // `F` must be generic.
+    fn apply<F>(f: F) where
+        F: Fn() {           // 对于接下来的调用，FnMut 或 FnOnce 都可以
+        f();
+    }
+
+    let x = 7;
+
+    // Capture `x` into an anonymous type and implement
+    // `Fn` for it. Store it in `print`.
+    let print = || println!("{}", x);
+
+    apply(print);
+}
+
+fn chapter9_2_4()
+{
+    // 疑问：这个是不是等效于前一节的写法？
+    // Define a function which takes a generic `F` argument
+    // bounded by `Fn`, and calls it
+    fn call_me<F: Fn()>(f: F) {
+        f();
+    }
+
+    // Define a wrapper function satisfying the `Fn` bound
+    fn function() {
+        println!("I'm a function!");
+    }
+
+    // Define a closure satisfying the `Fn` bound
+    let closure = || println!("I'm a closure!");
+
+    call_me(closure);
+    call_me(function);
+}
+
+fn chapter9_2_5()
+{
+    // 疑问：这三个方式有什么差别吗？
+    fn create_fn() -> impl Fn() {
+        let text = "Fn".to_owned();
+    
+        move || println!("This is a: {}", text)
+    }
+    
+    fn create_fnmut() -> impl FnMut() {
+        let text = "FnMut".to_owned();
+    
+        move || println!("This is a: {}", text)
+    }
+    
+    fn create_fnonce() -> impl FnOnce() {
+        let text = "FnOnce".to_owned();
+    
+        move || println!("This is a: {}", text)
+    }
+
+    let fn_plain = create_fn();
+    let mut fn_mut = create_fnmut();
+    let fn_once = create_fnonce();
+
+    fn_plain();
+    fn_mut();
+    fn_once();
+}
+
+fn chapter9_2_6_1()
+{
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    // `iter()` for vecs yields `&i32`. Destructure to `i32`.
+    println!("2 in vec1: {}", vec1.iter()     .any(|&x| x == 2));
+    // `into_iter()` for vecs yields `i32`. No destructuring required.
+    println!("2 in vec2: {}", vec2.into_iter().any(| x| x == 2));
+
+    // 到这里就只有 vec1 还能引用了，vec2 已经被 into_iter 消耗掉
+
+    let array1 = [1, 2, 3];
+    //let array2 = [4, 5, 6];
+
+    // `iter()` for arrays yields `&i32`.
+    println!("2 in array1: {}", array1.iter()     .any(|&x| x == 2));
+
+    // 注意：当前版本已经默认不允许对 array 使用 into_iter！
+    // `into_iter()` for arrays unusually yields `&i32`.
+    //println!("2 in array2: {}", array2.into_iter().any(|&x| x == 2));
+}
+
+fn chapter9_2_6_2()
+{
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    // `iter()` for vecs yields `&i32`.
+    let mut iter = vec1.iter();
+    // `into_iter()` for vecs yields `i32`.
+    let mut into_iter = vec2.into_iter();
+
+    // 疑问：没看懂为什么要双引用，以及为什么 find 和 any 的要求不一样
+
+    // `iter()` for vecs yields `&i32`, and we want to reference one of its
+    // items, so we have to destructure `&&i32` to `i32`
+    println!("Find 2 in vec1: {:?}", iter     .find(|&&x| x == 2));
+    // `into_iter()` for vecs yields `i32`, and we want to reference one of
+    // its items, so we have to destructure `&i32` to `i32`
+    println!("Find 2 in vec2: {:?}", into_iter.find(| &x| x == 2));
+
+    let array1 = [1, 2, 3];
+    //let array2 = [4, 5, 6];
+
+    // `iter()` for arrays yields `&i32`
+    println!("Find 2 in array1: {:?}", array1.iter()     .find(|&&x| x == 2));
+    // `into_iter()` for arrays unusually yields `&i32`
+    //println!("Find 2 in array2: {:?}", array2.into_iter().find(|&&x| x == 2));
+
+
+    let vec = vec![1, 9, 3, 3, 13, 2];
+
+    // 疑问：为什么这里连引用都不需要了？
+    let index_of_first_even_number = vec.iter().position(|x| x % 2 == 0);
+    assert_eq!(index_of_first_even_number, Some(5));
+    
+    let index_of_first_negative_number = vec.iter().position(|x| x < &0);
+    assert_eq!(index_of_first_negative_number, None);
+}
+
+fn chapter9_3()
+{
+    fn is_odd(n: u32) -> bool {
+        n % 2 == 1
+    }
+
+    println!("Find the sum of all the squared odd numbers under 1000");
+    let upper = 1000;
+
+    // Imperative approach
+    // Declare accumulator variable
+    let mut acc = 0;
+    // Iterate: 0, 1, 2, ... to infinity
+    for n in 0.. {
+        // Square the number
+        let n_squared = n * n;
+
+        if n_squared >= upper {
+            // Break loop if exceeded the upper limit
+            break;
+        } else if is_odd(n_squared) {
+            // Accumulate value, if it's odd
+            acc += n_squared;
+        }
+    }
+    println!("imperative style: {}", acc);
+
+    // Functional approach
+    let sum_of_squared_odd_numbers: u32 =
+        (0..).map(|n| n * n)                             // All natural numbers squared
+             .take_while(|&n_squared| n_squared < upper) // Below upper limit
+             .filter(|&n_squared| is_odd(n_squared))     // That are odd
+             .fold(0, |acc, n_squared| acc + n_squared); // Sum them
+    println!("functional style: {}", sum_of_squared_odd_numbers);
+}
+
+#[allow(dead_code)]
+#[allow(unreachable_code)]
+fn chapter9_4()
+{
+    fn foo() -> ! {
+        panic!("This call never returns.");
+    }
+
+
+    fn some_fn() {
+        ()
+    }
+
+    #[allow(unused_variables)]
+    let a: () = some_fn();
+    println!("This function returns and you can see this line.");
+
+    // 不能运行。。。提示 The `!` type is experimental
+    /*
+    #[feature(never_type)]
+    let x: ! = panic!("This call never returns.");
+    println!("You will never see this line!");
+    */
+
+    fn sum_odd_numbers(up_to: u32) -> u32 {
+        let mut acc = 0;
+        // 不包含 up_to
+        for i in 0..up_to {
+            // Notice that the return type of this match expression must be u32
+            // because of the type of the "addition" variable.
+            let addition: u32 = match i%2 == 1 {
+                // The "i" variable is of type u32, which is perfectly fine.
+                true => i,
+                // On the other hand, the "continue" expression does not return
+                // u32, but it is still fine, because it never returns and therefore
+                // does not violate the type requirements of the match expression.
+                false => continue,
+            };
+            acc += addition;
+        }
+        acc
+    }
+    println!("Sum of odd numbers up to 9 (excluding): {}", sum_odd_numbers(9));
+}
+
 fn main() {
     chapter9_0();
     chapter9_1();
     chapter9_2();
+    chapter9_2_1();
+    chapter9_2_2();
+    chapter9_2_3();
+    chapter9_2_4();
+    chapter9_2_5();
+    chapter9_2_6_1();
+    chapter9_2_6_2();
+    chapter9_3();
+    chapter9_4();
 }
